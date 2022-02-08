23823c3bfb3aa018b4f4cfea6805b5fc7ff47bdb
saabrin <90770758+saabrin@users.noreply.github.com>
2022-02-08 17:48:10 +0100

    Grocery list

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Keyboard,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Grocery from "./src/Grocery";
 
 export default function App() {
+  const [grocery, setGrocery] = useState();
+  const [groceryItem, setGroceryItem] = useState([]);
+  
+
+  const handleAddGrocery = () => {
+    /*tar bort keyborden när man lägger till en ny , på de mobila enheterna */
+    Keyboard.dismiss();
+    setGroceryItem([...groceryItem, grocery]);
+    setGrocery("");
+  };
+
+  const deletGrocery = (index) => {
+    let itemCopy = [...groceryItem];
+    itemCopy.splice(index, 1);
+    setGroceryItem(itemCopy);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yourTasks}></View>
+      <Text style={styles.title}>🍉 Your grocery list 🍉 </Text>
+      <View style={styles.items}>
+        {groceryItem.map((item, index) => {
+          return (
+            <TouchableOpacity key={index} onPress={() => deletGrocery(index)}>
+              <Grocery text={item} />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.textWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Type in your grocery here"}
+          placeholderTextColor="#FF3CBB"
+          value={grocery}
+          onChangeText={(text) => setGrocery(text)}
+        />
+
+        <TouchableOpacity onPress={() => handleAddGrocery()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.add}>☝️</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +67,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+  },
+  yourTasks: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FF3CBB",
+    textAlign: "center",
+  },
+  items: {
+    marginTop: 30,
+  },
+  textWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: "#FFA3DF",
+    borderRadius: 60,
+    borderColor: "#FF3CBB",
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 40,
+    height: 30,
+    backgroundColor: "#FFA3DF",
+    borderRadius: 10,
+    borderColor: "#FF3CBB",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  add: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#FF3CBB",
+    marginBottom: 3,
   },
 });
---
+++ src/Grocery.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+const Grocery = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>🍒 {props.text} </Text>
+      </View>
+      <View style={styles.circle}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FFA3DF",
+    padding: 15,
+    borderRadius: 10,
+    borderColor: "#FF3CBB",
+    borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#FF3CBB",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+    color: "#FF3CBB",
+  },
+  circle: {
+    width: 12,
+    height: 12,
+    borderColor: "#FF3CBB",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Grocery;
